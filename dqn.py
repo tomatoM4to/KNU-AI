795d6b12c96946f67f4e0cd85b9cd7293699728c
tomatoM4to <tomatomato2121@gmail.com>
2024-12-17 21:12:22 +0900

    first step

--- dqn.py
+++ dqn.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from collections import namedtuple, deque
 import random
-import matplotlib.pyplot as plt
 import numpy as np
+from typing import Tuple
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -39,3 +39,28 @@ class DQN(nn.Module):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         return self.layer3(x)
+
+
+def parse_state(state: list, target: list):
+    return np.array(
+        [state[0][0], state[0][1], state[0][-1], target[0], target[1], target[-1]]
+    )
+
+
+def calculate_reward(pre_state: list, state: list) -> Tuple[float, bool]:
+    x1_prev, y1_prev, sin1_prev, _, _, _ = pre_state
+    x1, y1, sin1, x2, y2, sin2 = state
+
+    # 보상 초기화
+    reward = 0.0
+
+    # x1이 x2에 1만큼 가까워지면 0.1 보상 추가
+    if abs(x1 - x2) < abs(x1_prev - x2):
+        reward += 0.1
+
+    # x1이 x2에 10 이내로 도달했을 때 종료 조건
+    if abs(x1 - x2) <= 10:
+        reward += 1.0
+        return reward, True
+
+    return reward, False
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import knu_rl_env as knu
 from knu_rl_env.road_hog import make_road_hog, RoadHogAgent, evaluate
-from dqn import DQN, ReplayMemory, Transition
+from dqn import DQN, ReplayMemory, Transition, parse_state, calculate_reward
 import numpy as np
 import torch
 import torch.optim as optim
 import torch.nn as nn
-import math
-import time
 from typing import Any, Dict
+import math
+import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+print(device)
 BATCH_SIZE = 128
 GAMMA = 0.99
 EPS_START = 0.9
@@ -19,19 +19,22 @@ EPS_DECAY = 1000
 TAU = 0.005
 LR = 1e-4
 
-env: Dict[str, Any] = make_road_hog(show_screen=False).reset()[0]
-observation: list = env["observation"]
-goal_spot: list = env["goal_spot"]
-is_on_load: bool = env["is_on_load"]
-is_crashed: bool = env["is_crashed"]
-o_time: int = env["time"]
+steps_done = 0
 
+ENV = make_road_hog(show_screen=False)
+observation: Dict[str, Any] = ENV.reset()[0]
+is_on_load: bool = observation["is_on_load"]
+is_crashed: bool = observation["is_crashed"]
+o_time: int = observation["time"]
 
-n_observations: int = 9
-n_actions: int = 9
+# 환경 초기화
+state: list = parse_state(observation["observation"], observation["goal_spot"])
+n_state: int = len(state)
+action_space = np.array([3, 4, 5])
+n_actions: int = len(action_space)
 
-policy_net = DQN(n_observations, n_actions).to(device)
-target_net = DQN(n_observations, n_actions).to(device)
+policy_net = DQN(n_state, n_actions).to(device)
+target_net = DQN(n_state, n_actions).to(device)
 target_net.load_state_dict(policy_net.state_dict())
 
 optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
@@ -39,29 +42,41 @@ memory = ReplayMemory(10000)
 
 
 class RoadHogRLAgent(RoadHogAgent):
-    toggle = False
+    def __init__(self):
+        super().__init__()
+        self.init_action_space = np.array([7, 7, 7, 3, 3, 7, 7, 7, 3, 3, 7, 7, 7])
+        self.idx = -1
+        self.loc = np.array([0, 0, 0])
+
+    def reset(self):
+        self.idx = -1
+
+    def env_reset(self):
+        for i in self.init_action_space:
+            ENV.step(i)
 
     def act(self, state):
-        self.toggle = not self.toggle
-        return 1 if self.toggle else 3
+        return 4
 
-    def train_act(self, state):
+    def train_act(self, state: torch.Tensor) -> torch.Tensor:
+        global steps_done
+
         sample = np.random.random()
 
         eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(
             -1.0 * steps_done / EPS_DECAY
         )
-
         steps_done += 1
         if sample > eps_threshold:
             with torch.no_grad():
                 return policy_net(state).max(1).indices.view(1, 1)
         else:
             return torch.tensor(
-                [[np.random.randint(9)]], device=device, dtype=torch.long
+                [[np.random.randint(3)]], device=device, dtype=torch.long
             )
 
-    def optimize_model():
+    def optimize_model(self):
+
         if len(memory) < BATCH_SIZE:
             return
         transitions = memory.sample(BATCH_SIZE)
@@ -102,36 +117,82 @@ class RoadHogRLAgent(RoadHogAgent):
         optimizer.step()
 
 
-def train():
-    agent = RoadHogRLAgent()
-    for episode in range(1000):
-        state, info = env.reset()
-        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-        episode_reward = 0
-        while 1:
-            action = agent.train_act(state)
-            observation, reward, terminated, truncated, _ = env.step(action.item())
-            episode_reward += reward
-            reward = torch.tensor([reward], device=device)
+def skip_step(
+    action: torch.Tensor,
+):
+    done = False
+    a = action_space[action.item()]
+    for _ in range(3):
+        observation, reward, terminated, truncated, _ = ENV.step(a)
+        done = terminated or truncated
+        if done:
+            break
+    return observation, done
 
-            if terminated:
+
+rewards_history = []
+
+
+def train(agent: RoadHogRLAgent):
+    observation: Dict[str, Any]
+    done: bool
+    goal: bool
+    state: list
+    pre_state: list
+    state_t: torch.Tensor
+    next_state: torch.Tensor | None
+    reward: int
+    reward_t: torch.Tensor
+    action: torch.Tensor
+
+    for episode in range(600):
+        agent.reset()
+        observation, _ = ENV.reset()
+        pre_state = parse_state(observation["observation"], observation["goal_spot"])
+        state_t = torch.tensor(pre_state, dtype=torch.float32, device=device).unsqueeze(
+            0
+        )
+
+        episode_reward = 0
+
+        agent.env_reset()
+        while 1:
+            action = agent.train_act(state_t)
+            observation, done = skip_step(action)
+            state = parse_state(observation["observation"], observation["goal_spot"])
+            reward, goal = calculate_reward(pre_state, state)
+
+            # 보상 관련 처리
+            if done:
+                reward = -1.0
+            elif not observation["is_on_load"]:
+                reward = -1.0
+                done = True
+
+            if goal:
+                done = True
+
+            episode_reward += reward
+            reward_t = torch.tensor([reward], device=device)
+
+            # 다음 상태를 위한 처리
+            if done:
                 next_state = None
             else:
                 next_state = torch.tensor(
-                    observation, dtype=torch.float32, device=device
+                    state, dtype=torch.float32, device=device
                 ).unsqueeze(0)
 
             # 메모리에 변이 저장
-            memory.push(state, action, next_state, reward)
+            memory.push(state_t, action, next_state, reward_t)
 
             # 다음 상태로 이동
-            state = next_state
+            state_t = next_state
 
             # (정책 네트워크에서) 최적화 한단계 수행
             agent.optimize_model()
 
             # 목표 네트워크의 가중치를 소프트 업데이트
-            # θ′ ← τ θ + (1 −τ )θ′
             target_net_state_dict = target_net.state_dict()
             policy_net_state_dict = policy_net.state_dict()
             for key in policy_net_state_dict:
@@ -140,13 +201,23 @@ def train():
                 ] * TAU + target_net_state_dict[key] * (1 - TAU)
             target_net.load_state_dict(target_net_state_dict)
 
-            if terminated or truncated:
+            if done:
                 break
+
+        rewards_history.append(episode_reward)
+        if episode % 50 == 0:
+            print(f"episode: {episode} reward: {episode_reward}")
+            print(f"agent x: {state[0]} goal x: {state[3]}")
 
 
 if __name__ == "__main__":
     agent = RoadHogRLAgent()
-    evaluate(agent)
-    # train()
+    # evaluate(agent)
+    train(agent)
     # knu.road_hog.run_manual()
-    pass
+    print("Complete")
+    plt.plot(range(len(rewards_history)), rewards_history, color="blue")
+    plt.xlabel("Episode")
+    plt.ylabel("Reward")
+    plt.title("Rewards per Episode")
+    plt.savefig("dqn-reward-history.png")
